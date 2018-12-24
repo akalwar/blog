e5f1b06a26458713d87a5b794e1e42697705a83f
Abhishek Kalwar <abhishekkalwar835@gmail.com>
2018-12-24 13:29:20 +0530

    added heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kc0&mdsy=f#z)%xcr7ogcaz^7rf15skhi-brwdz=ietf(vng(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
